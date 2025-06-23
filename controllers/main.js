4c7bbd6090908626d56b2b2dc8939254426168bd
Adham Mustafa <adhoomy.mustafa@gmail.com>
2025-06-22 21:55:06 -0400

    Created separate custom error classes.

--- controllers/main.js
+++ controllers/main.js
@@ -1,10 +1,10 @@
 const jwt = require('jsonwebtoken')
-const CustomAPIError = require('../errors/custom-error')
+const {BadRequestError} = require('../errors')
 
 const login = async (req, res) => {
     const {username, password} = req.body
     if (!username || !password) {
-        throw new CustomAPIError('Please provide email and password', 400)
+        throw new BadRequestError('Please provide email and password')
     }
     // not really an ID
     const id = new Date().getDate()
@@ -13,8 +13,6 @@ const login = async (req, res) => {
 }
 
 const dashboard = async (req, res) => {
-    console.log(req.user)
-
     const luckyNumber = Math.floor(Math.random() * 100)
     res.status(200).json({
         msg: `Hello, ${req.user.username}`,
